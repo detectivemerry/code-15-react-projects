3969c99effb5e7bd865243f48b0f4be23ce1fd41
Teo Jia Sheng <jiiasheng@hotmail.com>
2021-12-22 23:38:29 +0800

    part 4 completed

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  //iterate over all qn
+  //display single Question
+  const [index, setIndex] = useState(0)
+
+  return(
+    <div className = 'container'>
+      <h3>Questions And Answers About Login</h3>
+      <section className = 'info'>
+        {data.map((question)=>{
+          return <SingleQuestion key = {question.id} {...question}/>
+        })}
+      </section>
+    </div>
+  )
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info}) => {
+
+  const [readMore, setReadMore] = useState(false)
+
+  return (
+    <div className = 'question'>
+      <header>
+      <h4>{title}</h4>
+      <button className = 'btn' onClick = {()=>{setReadMore(!readMore)}}><AiOutlineMinus /></button>
+      </header>
+      
+      {readMore?info : ''}
+      </div>
+  )
+  
 };
 
 export default Question;
